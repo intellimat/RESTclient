6cbbc69b68f691cfeb2b1f37b2c52e54caf28c1f
intellimat <38228915+intellimat@users.noreply.github.com>
2020-05-05 15:44:43 +0200

    Implemented first get request

--- client.py
+++ client.py
@@ -1,35 +1,42 @@
 import json
-import socket
+import urllib.request
 
 ####################    Methods' definitions    ####################
-def connectTo(IP, PORT): #tries to reach a server running on IP and PORT specified
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((IP, PORT))
-    return s
-
-def readResponse(socket): #reads the message coming from the client
-    rawData = socket.recv(8192)
-    decodedData = rawData.decode("utf-8")
-    return decodedData
-
-def closeConnection(socket): #closes a TCP connection reachable through the socket argument
-    host, port = socket.getpeername()
-    socket.close()
-    print(f'\nThe connection to {host} on port {port} has been closed by the server')
-
-def writeGETrequest(s):
-    host, port = socket.getpeername()
-    socket.send(bytes(s,"utf-8"))
-
 def getJSONobjectFromString(s):
     return json.loads(s)
 
-def getBodyFromHTTPresponse(serverResponse):
-    body = serverResponse.split('\r\n\r\n')[1]
-    return body
+def showAllUAbuildings():
+    contents = urllib.request.urlopen('https://www.sigua.ua.es/api/pub/edificio/all/items').read()
+    json = getJSONobjectFromString(contents)
+    for item in json:
+        print(f"ID: {item['id']}\nNombre: {item['nombre']}\nPlantas: {item['plantas']}\n\n")
+
+def askValidInput():
+    validInputs = ['0','1','2','3','4']
+    valid = False
+    while not valid:
+        inputValue = input("\nSelect an option\n\n" +
+                "0: exit\n\n" +
+                "1: Listado de todos los edificios de la Universidad de Alicante\n\n" +
+                "2: Listado de datos de volumen de todos los edificios de la Universidad de Alicante\n\n" +
+                "3: Listado de las estancias de un edificio concreto de la Universidad de Alicante\n\n" +
+                "4: Listado de departamentos de la Universidad de Alicante\n\n\n")
+        if inputValue in validInputs:
+            valid = True
+            return inputValue
+        else:
+            print('\nNOT VALID INPUT. It must be an integer between 0 and 4. \n\n')
 
 ####################    Here the execution starts    ####################
-try:
-    connectTo('www.sigua.ua.es', 80)
-except socket.error as e:
-    print('The client could not established a connection to the web server.\n')
+#uaSocket = connectTo('www.sigua.ua.es', 80)
+inputValue = askValidInput()
+while inputValue !='0':
+    if inputValue == '1':
+        showAllUAbuildings()
+    elif inputValue == '2':
+        pass
+    elif inputValue == '3':
+        pass
+    else:   #input==4
+        pass
+    inputValue = askValidInput()
